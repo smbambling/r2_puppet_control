95ef773be7675250d130d9c27e14504b0c51ba1c
Steven Bambling <smbambling@gmail.com>
2016-10-06 13:07:43 -0400

    adding local_firewall

--- site/role/manifests/r2.pp
+++ site/role/manifests/r2.pp
@@ -1,4 +1,4 @@
-# == Overview: R2 Management 
+# == Overview: R2 Management
 #
 # == Monitoring: No monitoring is currently provided
 #
@@ -7,9 +7,11 @@
 class role::r2 {
 
   contain profile::base
+  contain profile::local_firewall
   contain profile::plex_media_server
 
   Class['profile::base'] ->
+  Class['profile::local_firewal'] ->
   Class['profile::plex_media_server']
 
 }
