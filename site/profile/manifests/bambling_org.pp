6951e7b8def2c97bbc65af2bad528c4c473d0fad
Steven Bambling <smbambling@gmail.com>
2016-10-07 12:06:30 -0400

    adding bambling.org management

---
+++ site/profile/manifests/bambling_org.pp
@@ -0,0 +1,33 @@
+# == Overview: Manage bambling.org Website
+#
+# == Requirements:
+#
+# == Monitoring: No monitoring is currently provided
+#
+# == Notes:
+class profile::bambling_org (
+  Boolean $monitoring = hiera('base::monitoring', true),
+  String $ssl_cert    = '/etc/ssl/certs/bambling.org-server.crt',
+  String $ssl_key     = '/etc/pki/tls/private/bambling.org-server.key',
+){
+
+  apache::vhost { 'bambling.org-non-ssl' :
+    port            => '80',
+    docroot         => '/var/www/html',
+    servername      => 'bambling.org',
+    redirect_status => 'permanent',
+    redirect_dest   => 'https://bambling.org/',
+  }
+
+  apache::vhost { 'bambling.org-ssl':
+    servername => 'bambling.org',
+    port       => '443',
+    docroot    => '/var/www/html',
+    ssl        => true,
+    ssl_cert   => $ssl_cert,
+    ssl_key    => $ssl_key,
+  }
+
+}
+
+
