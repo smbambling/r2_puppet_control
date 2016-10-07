02b73a714ac98e0405c0b68889e585ca72dd9327
Steven Bambling <smbambling@gmail.com>
2016-10-07 12:35:52 -0400

    calling new classes in role

--- site/role/manifests/r2.pp
+++ site/role/manifests/r2.pp
@@ -9,9 +9,13 @@ class role::r2 {
   contain profile::base
   contain profile::local_firewall
   contain profile::plex_media_server
+  contain profile::apache
+  contain profile::bambling_org
 
   Class['profile::base'] ->
   Class['profile::local_firewall'] ->
+  Class['profile::apache'] ->
+  Class['profile::bambling_org'] ->
   Class['profile::plex_media_server']
 
 }
