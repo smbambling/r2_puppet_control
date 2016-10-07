e3d9be578f0d5852c72472312909e9dc6c539f64
Steven Bambling <smbambling@gmail.com>
2016-10-07 12:34:10 -0400

    updating docroot for bambling.org

--- site/profile/manifests/bambling_org.pp
+++ site/profile/manifests/bambling_org.pp
@@ -9,11 +9,14 @@ class profile::bambling_org (
   Boolean $monitoring = hiera('base::monitoring', true),
   String $ssl_cert    = '/etc/pki/tls/certs/wildcard.bambling.org-server.crt',
   String $ssl_key     = '/etc/pki/tls/private/wildcard.bambling.org-server.key',
+  String $docroot     = '/vol0/www/bambling_org/main',
 ){
+
+  # Need to make this create /vol0/www/bambling_org
 
   apache::vhost { 'bambling.org-non-ssl' :
     port            => '80',
-    docroot         => '/var/www/html',
+    docroot         => $docroot,
     servername      => 'bambling.org',
     #redirect_status => 'permanent',
     #redirect_dest   => 'https://bambling.org/',
@@ -22,7 +25,7 @@ class profile::bambling_org (
   apache::vhost { 'bambling.org-ssl':
     servername => 'bambling.org',
     port       => '443',
-    docroot    => '/var/www/html',
+    docroot    => $docroot,
     ssl        => true,
     ssl_cert   => $ssl_cert,
     ssl_key    => $ssl_key,
