4c7646b7f2b6f474701038461ca5ed0e0dbc7d0e
Steven Bambling <smbambling@gmail.com>
2016-10-09 08:13:06 -0400

    adding techtaco.org

---
+++ site/profile/manifests/techtaco_org/kb.pp
@@ -0,0 +1,35 @@
+# == Overview: Manage techtaco.org Website
+#
+# == Requirements:
+#
+# == Monitoring: No monitoring is currently provided
+#
+# == Notes:
+class profile::techtaco_org::kb (
+  Boolean $monitoring = hiera('base::monitoring', true),
+  String $ssl_cert    = '/etc/pki/tls/certs/wildcard.techtaco.org-server.crt',
+  String $ssl_key     = '/etc/pki/tls/private/wildcard.techtaco.org-server.key',
+  String $docroot     = '/vol0/www/techtaco_org/mdwiki',
+  String $site_name   = 'kb.techtaco.org',
+){
+
+  # Need to make this create /vol0/www/techtaco_org
+
+  apache::vhost { "${site_name}-non-ssl" :
+    port            => '80',
+    docroot         => $docroot,
+    servername      => $site_name,
+    #redirect_status => 'permanent',
+    #redirect_dest   => 'https://techtaco.org/',
+  }
+
+  apache::vhost { "${site_name}-ssl":
+    servername => $site_name,
+    port       => '443',
+    docroot    => $docroot,
+    ssl        => true,
+    ssl_cert   => $ssl_cert,
+    ssl_key    => $ssl_key,
+  }
+
+}
