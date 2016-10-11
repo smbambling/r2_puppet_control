ae451d4924f00f92bd0e73a10e4961254a12061a
jones01 <octavis.jones@gmail.com>
2016-10-11 14:16:20 -0400

    adding role

--- site/role/manifests/juicebox.pp
+++ site/role/manifests/juicebox.pp
@@ -10,10 +10,12 @@ class role::juicebox {
   contain profile::local_firewall
   contain profile::plex_media_server
   contain profile::apache
+  contain profile::octavisjones_com::main
 
   Class['profile::base'] ->
   Class['profile::local_firewall'] ->
   Class['profile::apache'] ->
+  Class['profile::octavisjones_com::main'] ->  
   Class['profile::plex_media_server']
 
 }
