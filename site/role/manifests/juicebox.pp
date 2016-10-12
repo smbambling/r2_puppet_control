8f03fa59a5fc5429abd9cc92ef4129bf715ab8db
jones01 <octavis.jones@gmail.com>
2016-10-12 14:39:03 -0400

    adding graphite config

--- site/role/manifests/juicebox.pp
+++ site/role/manifests/juicebox.pp
@@ -10,11 +10,13 @@ class role::juicebox {
   contain profile::local_firewall
   contain profile::plex_media_server
   contain profile::apache
+  contain profile::graphite
   contain profile::octavisjones_com::main
 
   Class['profile::base'] ->
   Class['profile::local_firewall'] ->
   Class['profile::apache'] ->
+  Class['profile::grpahite'} ->
   Class['profile::octavisjones_com::main'] ->  
   Class['profile::plex_media_server']
 
