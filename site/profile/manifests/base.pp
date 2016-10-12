639bba8e77f92efb9e145b4b96b2ac55d3963e86
Steven Bambling <smbambling@gmail.com>
2016-10-12 18:38:29 -0400

    removing ohmyzsh

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -25,8 +25,6 @@ class profile::base (
   package { 'zsh':
     ensure => present,
   }
-  # Install OhMyZSH per user if specified
-  contain ::profile::ohmyzsh
 
   # Set sexlinux to permissive
   class { selinux:
